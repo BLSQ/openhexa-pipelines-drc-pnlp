31a8927703a0c8bf3ca3dfcaa7e681e8e4038210
EMontandon <esteban14m@gmail.com>
2025-01-20 15:26:22 +0100

    extra OUG list update task

--- sentinel_update_extract/pipeline.py
+++ sentinel_update_extract/pipeline.py
@@ -33,52 +33,80 @@ from openhexa.sdk import current_run, pipeline, parameter, workspace
 def sentinel_update_extract(get_year, update_pnlp):
     """
     In this pipeline we call a notebook that executes the sentinel extract update
-    
+
     """
 
     # Setup variables
-    notebook_name = "sentinel_data_from_routine_extracts_pipeline"    
+    notebook_name = "sentinel_data_from_routine_extracts_pipeline"
     notebook_path = f"{workspace.files_path}/sentinel-sites/code/"
     out_notebook_path = f"{workspace.files_path}/sentinel-sites/papermill_outputs"
 
     # Set parameters
     parameters = {
-        'ANNEE_A_ANALYSER': get_year,
+        "ANNEE_A_ANALYSER": get_year,
         # 'UPLOAD_LEGACY' : update_legacy,
-        'UPLOAD_PNLP' : update_pnlp        
+        "UPLOAD_PNLP": update_pnlp,
     }
 
-    # Run update notebook for PNLP tables    
-    run_update_with(nb_name=notebook_name, nb_path=notebook_path, out_nb_path=out_notebook_path, parameters=parameters) 
+    # execute OU sentinel uid list (NOTE: see sentinel-sites/sentinel_org_units_groups.ipynb)
+    sucess = run_sentinel_oug_update(parameters=parameters)
+
+    # Run update notebook for PNLP tables
+    run_update_with(
+        nb_name=notebook_name,
+        nb_path=notebook_path,
+        out_nb_path=out_notebook_path,
+        parameters=parameters,
+        sucess=sucess,
+    )
 
 
 @sentinel_update_extract.task
-def run_update_with(nb_name:str, nb_path:str, out_nb_path:str, parameters:dict):
+def run_update_with(nb_name: str, nb_path: str, out_nb_path: str, parameters: dict, succes: bool):
     """
     Update a tables using the latest dataset version
-    
-    """         
+
+    """
     nb_full_path = os.path.join(nb_path, f"{nb_name}.ipynb")
-        
+
     current_run.log_info(f"Executing sentinel update notebook: {nb_full_path}")
     current_run.log_info(f"Running report for YEAR: {parameters['ANNEE_A_ANALYSER']}")
     current_run.log_info(f"Database update PNLP: {parameters['UPLOAD_PNLP']}")
 
     # out_nb_fname = os.path.basename(in_nb_dir.replace('.ipynb', ''))
-    execution_timestamp = datetime.utcnow().strftime("%Y-%m-%d_%H_%M_%S")   
-    out_nb_fname = f"{nb_name}_OUTPUT_{execution_timestamp}.ipynb" 
+    execution_timestamp = datetime.utcnow().strftime("%Y-%m-%d_%H_%M_%S")
+    out_nb_fname = f"{nb_name}_OUTPUT_{execution_timestamp}.ipynb"
     out_nb_full_path = os.path.join(out_nb_path, out_nb_fname)
 
-    try:            
-        pm.execute_notebook(input_path = nb_full_path,
-                            output_path = out_nb_full_path,
-                            parameters=parameters)
+    try:
+        pm.execute_notebook(input_path=nb_full_path, output_path=out_nb_full_path, parameters=parameters)
     except Exception as e:
-        current_run.log_info(f'Caught error {type(e)}: e')
+        current_run.log_info(f"Caught error {type(e)}: e")
 
-    current_run.log_info(f"Sentinel table updated")
+    current_run.log_info("Sentinel table updated")
 
- 
+
+@sentinel_update_extract.task
+def run_sentinel_oug_update(parameters: dict):
+    """
+    We update the organisation unit groups file for the current year that
+    should contain the list of organisation units for which we compute the
+    sentinel indicators.
+    """
+    execution_timestamp = datetime.utcnow().strftime("%Y-%m-%d_%H_%M_%S")
+    try:
+        pm.execute_notebook(
+            input_path=f"{workspace.files_path}/sentinel-sites/sentinel_org_units_groups_OUTPUT_{execution_timestamp}.ipynb",
+            output_path=f"{workspace.files_path}/sentinel-sites/papermill_outputs",
+            parameters=parameters,
+        )
+    except Exception as e:
+        current_run.log_info(f"Sentinel OU groups update error {type(e)}: e")
+
+    current_run.log_info("Sentinel organisation unit groups file updated.")
+
+    return True
+
 
 if __name__ == "__main__":
     sentinel_update_extract()
